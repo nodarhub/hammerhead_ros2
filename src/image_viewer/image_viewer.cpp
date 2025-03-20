abd8f405ee34dd35902a62b1f92e9ab96bc2aad8
mholzel <mholzel@users.noreply.github.com>
2025-03-20 15:19:04 +0100

    Update the image viewer to work with more image types

--- src/image_viewer/image_viewer.cpp
+++ src/image_viewer/image_viewer.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 
 void signalHandler(int signum) {
     std::cerr << "SIGINT or SIGTERM received." << std::endl;
     std::exit(EXIT_FAILURE);
+}
+
+int getCvType(const std::string &encoding, bool &type_error) {
+    if (encoding == "bayer_bggr8" || encoding == "mono8") {
+        return CV_8UC1;
+    } else if (encoding == "bgr8") {
+        return CV_8UC3;
+    } else if (encoding == "bgra8") {
+        return CV_8UC4;
+    } else if (encoding == "bayer_bggr16" || encoding == "mono16") {
+        return CV_16UC1;
+    } else if (encoding == "bgr16") {
+        return CV_16UC3;
+    } else if (encoding == "bgra16") {
+        return CV_16UC4;
+    }
+    type_error = true;
+    return -1;
 }
 
 class Ros2ImageViewer : public rclcpp::Node {
@@ -29,21 +48,29 @@ private:
             stop_promise.set_value();
             return;
         }
-        if (image.rows != msg->height or image.cols != msg->width) {
-            std::cout << "Cached image is the wrong size. Resizing to " << msg->width << "x" << msg->height
-                      << " with the type " << msg->encoding << std::endl;
-            if (msg->encoding == "bgr8") {
-                image = cv::Mat(msg->height, msg->width, CV_8UC3);
-            } else if (msg->encoding == "mono16") {
-                image = cv::Mat(msg->height, msg->width, CV_16UC1);
-            } else {
-                std::cerr << "Unknown image encoding `" << msg->encoding << "`\n";
-                stop_promise.set_value();
-                return;
-            }
+        // Determine the CV type from the encoding
+        bool type_error = false;
+        const auto cv_type = getCvType(msg->encoding, type_error);
+        if (type_error) {
+            std::cerr << "Unknown image encoding `" << msg->encoding << "`\n";
+            stop_promise.set_value();
+            return;
         }
+        if (image.rows != msg->height or image.cols != msg->width or cv_type != image.type()) {
+            std::cout << "Cached image is the wrong size or type. "  //
+                      << "Resizing to " << msg->width << "x" << msg->height  //
+                      << " with the type " << msg->encoding << std::endl;
+            image = cv::Mat(msg->height, msg->width, cv_type);
+        }
+        // Copy the data into the cv::Mat
         const auto size = msg->height * msg->step;
         std::copy(msg->data.data(), msg->data.data() + size, image.data);
+
+        // If the encoding is a Bayer pattern, convert to BGR
+        if (msg->encoding == "bayer_bggr8" or msg->encoding == "bayer_bggr16") {
+            cv::cvtColor(image, image, cv::COLOR_BayerBG2BGR);
+        }
+
         // Downsize the image before viewing
         cv::resizeWindow(topic, {640, 480});
         cv::imshow(topic, image);
